b16f863a1c5c00cc182e40bc590300748fe53ca0
Homework1 <kalinina_en@hotmail.com>
2023-11-01 23:43:12 +0300

    add new class Primitives

---
+++ src/main/java/Primitives.java
@@ -0,0 +1,20 @@
+public class Primitives {
+    public static void main(String[] args) {
+        byte i = 127;
+        System.out.println("byte = " + i);
+        short b = 32000;
+        System.out.println("short = " + b);
+        int c = 2000000;
+        System.out.println("int = " + c);
+        long d = 15000000000L;
+        System.out.println("long = " + d);
+        float e = 5.1111111f;
+        System.out.println("float = " + e);
+        double f = 5.111111111111111111;
+        System.out.println("double = " + f);
+        char g = 'A';
+        System.out.println("char = " + g);
+        boolean isGood = true;
+        System.out.println("boolean = " + isGood);
+    }
+}
