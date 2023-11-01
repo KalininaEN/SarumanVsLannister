e92bae8841b2d751c5863a4298dab6d71cb59c46
Homework1 <kalinina_en@hotmail.com>
2023-11-01 23:36:37 +0300

    add new class Primitives

--- src/main/java/SarumanVsLannisters.java
+++ src/main/java/SarumanVsLannisters.java
@@ -1,10 +1,13 @@
 public class SarumanVsLannisters {
 
     public static void main(String[] args) {
+
         final int warriorsOfLannister = 15746;
         System.out.println("warriorsOfLannister = " + warriorsOfLannister);
+
         final int orcsOfSaruman = 9567;
         System.out.println("orcsOfSaruman = " + orcsOfSaruman);
+
         final int allArmy = warriorsOfLannister + orcsOfSaruman;
         System.out.println("allArmy = " + allArmy);
     }
