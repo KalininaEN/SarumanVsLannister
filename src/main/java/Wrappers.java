0b874da63a90dcaee9825c8389183a66616e98ad
Homework1 <kalinina_en@hotmail.com>
2023-11-02 00:09:57 +0300

    add new class Primitives add new class Wrappers

---
+++ src/main/java/Wrappers.java
@@ -0,0 +1,13 @@
+public class Wrappers {
+    public static void main(String[] args) {
+
+        Byte a = 127;
+        Short b = 32000;
+        Integer c = 2000000;
+        Long d = 14000000000000L;
+        Float e = 5.1111111f;
+        Double f = 5.1111;
+        Character g = 'A';
+        Boolean isBad = false;
+    }
+}
