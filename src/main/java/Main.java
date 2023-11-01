597b450a22da3d5253b956160c82454068e200da
Homework1 <kalinina_en@hotmail.com>
2023-11-02 01:35:27 +0300

    add new class Primitives add new class Wrappers Add new classes: Animal, Cat, Fish, Duck and Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.move();
+
+        Fish fish = new Fish();
+        fish.move();
+
+        Duck duck = new Duck();
+        duck.move();
+    }
+}
